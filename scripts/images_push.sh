d2d07ad9c5baf36f734ab91b688222b14860da9b
Samuel2402 <84233173+Samuel2402@users.noreply.github.com>
2021-08-12 12:24:31 +0100

    Update images_push.sh

--- scripts/images_push.sh
+++ scripts/images_push.sh
@@ -26,7 +26,7 @@ output "Pushing qa/petclinic-fe to dockerhub"
 
 if [[ ! "$(docker images -q qa/petclinic-fe:latest 2> /dev/null)" == "" ]]; then
     output "Image exists"
-    sudodocker push samuel240210/pet-clinic qa/petclinic-fe
+    sudo docker push samuel240210/pet-clinic qa/petclinic-fe
 fi
 
 output "Pushing qa/petclinic-be to dockerhub"
