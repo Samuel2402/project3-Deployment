8fa90fc982870a73040c90698140b69470331666
Samuel2402 <84233173+Samuel2402@users.noreply.github.com>
2021-08-12 15:55:02 +0100

    Update build_images.sh

--- scripts/build_images.sh
+++ scripts/build_images.sh
@@ -26,7 +26,7 @@ output "Building the Petclinic FE"
 cd ./spring-petclinic-angular/; npm install; npm run-script build; cd ..
 
 output "Building spring-petclinic-angular"
-docker build --no-cache -t qa/petclinic-fe ./spring-petclinic-angular
+docker build --no-cache -t samuel240210/petclinic-fe ./spring-petclinic-angular
 
 if [[ "$(docker images -q qa/petclinic-fe:latest 2> /dev/null)" == "" ]]; then
     error "could not rebuild image"
@@ -36,14 +36,14 @@ output "Running maven clean install to rebuild the jar file"
 cd ./spring-petclinic-rest/; mvn clean install; cd ..
 
 output "Building spring-petclinic-rest"
-docker build --no-cache -t qa/petclinic-be ./spring-petclinic-rest
+docker build --no-cache -t samuel240210/petclinic-be ./spring-petclinic-rest
 
 if [[ "$(docker images -q qa/petclinic-be:latest 2> /dev/null)" == "" ]]; then
     error "could not rebuild image"
 fi
 
 output "Building nginx image"
-docker build --no-cache -t qa/nginx ./NGINX
+docker build --no-cache -t samuel240210/nginx ./NGINX
 
 if [[ "$(docker images -q qa/nginx:latest 2> /dev/null)" == "" ]]; then
     error "could not rebuild image"
